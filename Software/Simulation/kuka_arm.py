e31ccc93ace42647c4040d32cfc20290063d0917
lok-i <lokesha1b2c3@gmail.com>
2020-07-06 01:33:12 +0530

    custom edited version of kuka+wsg50 arm, under testing

--- Software/Simulation/kuka_arm.py
+++ Software/Simulation/kuka_arm.py
@@ -1,6 +1,6 @@
 from pybullet_envs.bullet.kukaGymEnv import KukaGymEnv
 import os
-from imageai.Detection.Custom import DetectionModelTrainer,CustomObjectDetection
+#from imageai.Detection.Custom import DetectionModelTrainer,CustomObjectDetection
 import cv2
 import random
 import os
@@ -17,11 +17,11 @@ from pkg_resources import parse_version
 import gym
 from kuka_discrete_obj_env import KukaDiverseObjectEnv
 
-detector=CustomObjectDetection()
-detector.setModelTypeAsYOLOv3()
-detector.setModelPath('detection_model-ex-004--loss-0004.076.h5')
-detector.setJsonPath('detection_config.json')
-detector.loadModel()
+# detector=CustomObjectDetection()
+# detector.setModelTypeAsYOLOv3()
+# detector.setModelPath('detection_model-ex-004--loss-0004.076.h5')
+# detector.setJsonPath('detection_config.json')
+# detector.loadModel()
 
 env=KukaDiverseObjectEnv()
 obs=env.reset()
@@ -33,6 +33,7 @@ centroids=[]
 def object_detection():
     image=env._get_observation()
     cv2.imwrite("image.jpeg",image)
+    '''
     detections=detector.detectObjectsFromImage(input_image='image.jpeg',output_image_path='res.jpeg')
     res=cv2.imread('res.jpeg')
 
@@ -50,7 +51,8 @@ def object_detection():
             y=y-0.02
         #print(x,y)
         centroids.append((x,y))
-object_detection()
+        '''
+#object_detection()
 i=0
 x=centroids[i][0]
 y=centroids[i][1]
@@ -61,7 +63,7 @@ while True:
     observation,reward,done,_=env.step([(x-actualEndEffectorPos[0])*10,(y-actualEndEffectorPos[1])*10,z,0,0])
     if done==True:
         centroids=[]
-        object_detection()
+        #object_detection()
         env._attempted_grasp = False
         env._env_step = 0
         env.terminated = 0
--- Software/Simulation/visualizer.py
+++ Software/Simulation/visualizer.py
@@ -1,6 +1,7 @@
 import pybullet as p
 import time
 import math
+import numpy as np
 from datetime import datetime
 import pybullet_data
 
@@ -10,38 +11,29 @@ if (clid < 0):
   #p.connect(p.SHARED_MEMORY_GUI)
 
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
-gripper_list= p.loadSDF("./gripper/wsg50_one_motor_gripper.sdf")
-gripper = gripper_list[0]
-p.resetBasePositionAndOrientation(gripper,[1,0,0],p.getQuaternionFromEuler([0,0,1.57]))
 
-for i in range(p.getNumJoints(gripper)):
-  print('i:',p.getJointState(gripper,i))
+
+
 
 p.loadURDF("plane.urdf", [0, 0, 0])
-p.loadURDF("./SCARA/scara_robot_left.urdf",[2,0,0])
-kukaId = p.loadURDF("kuka_iiwa/model.urdf", [0, 0, 0])
-p.resetBasePositionAndOrientation(kukaId, [0, 0, 0], [0, 0, 0, 1])
-kukaEndEffectorIndex = 6
-#p.createConstraint(kukaId,6,gripper,0,p.JOINT_FIXED,[0,0,0],[0,0,0],[0,0,0])
-numJoints = p.getNumJoints(kukaId)
-if (numJoints != 7):
-  exit()
 
-#lower limits for null space
 ll = [-.967, -2, -2.96, 0.19, -2.96, -2.09, -3.05]
-#upper limits for null space
+    #upper limits for null space
 ul = [.967, 2, 2.96, 2.29, 2.96, 2.09, 3.05]
-#joint ranges for null space
+    #joint ranges for null space
 jr = [5.8, 4, 5.8, 4, 5.8, 4, 6]
-#restposes for null space
+    #restposes for null space
 rp = [0, 0, 0, 0.5 * math.pi, 0, -math.pi * 0.5 * 0.66, 0]
-#joint damping coefficents
-jd = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
+    #joint damping coefficents
+jd = [
+        0.00001, 0.00001, 0.00001, 0.00001, 0.00001, 0.00001, 0.00001, 0.00001, 0.00001, 0.00001,
+        0.00001, 0.00001, 0.00001, 0.00001
+    ]
 
-for i in range(numJoints):
-  p.resetJointState(kukaId, i, rp[i])
 
-p.setGravity(0, 0, 0)
+kukaEndEffectorIndex = 14
+
+p.setGravity(0, 0, -10)
 t = 0.
 prevPose = [0, 0, 0]
 prevPose1 = [0, 0, 0]
@@ -54,83 +46,53 @@ useOrientation = 1
 useSimulation = 1
 useRealTimeSimulation = 0
 ikSolver = 0
-p.setRealTimeSimulation(useRealTimeSimulation)
 #trailDuration is duration (in seconds) after debug lines will be removed automatically
 #use 0 for no-removal
-trailDuration = 15
+#trailDuration = 15
+def draw_frame(point,draw_target=False):
+  if draw_target:
+    p.addUserDebugLine(point,[point[0]+0.5,point[1],point[2]],[0,1,0],5)
+    p.addUserDebugLine(point,[point[0],point[1]+0.5,point[2]],[0,1,0],5)
+    p.addUserDebugLine(point,[point[0],point[1],point[2]+0.5],[0,1,0],5)
+  else:
+    p.addUserDebugLine(point,[point[0]+0.3,point[1],point[2]],[1,0,0],5)
+    p.addUserDebugLine(point,[point[0],point[1]+0.3,point[2]],[0,1,0],5)
+    p.addUserDebugLine(point,[point[0],point[1],point[2]+0.3],[0,0,1],5)
 
 i=0
-'''
+radius = 0.6
+reverse = False
+grip_list= p.loadSDF("./kukka_wsg50/kuka_with_wsg50.sdf")
+gripper1 = grip_list[0]
+p.resetBasePositionAndOrientation(gripper1,[0,0,0],p.getQuaternionFromEuler([0,0,1.57]))
+
+grip_list2= p.loadSDF("./kukka_wsg50/kuka_with_wsg50.sdf")
+gripper2 = grip_list2[0]
+p.resetBasePositionAndOrientation(gripper2,[1,0,0],p.getQuaternionFromEuler([0,0,1.57]))
 while 1:
-  i+=1
-  #p.getCameraImage(320,
-  #                 200,
-  #                 flags=p.ER_SEGMENTATION_MASK_OBJECT_AND_LINKINDEX,
-  #                 renderer=p.ER_BULLET_HARDWARE_OPENGL)
-  if (useRealTimeSimulation):
-    dt = datetime.now()
-    t = (dt.second / 60.) * 2. * math.pi
+  
+  #pos = [-radius*math.cos(t), radius*math.sin(t) ,0.5 ]
+  pos_1 = [0,0.7,0.5]
+  pos_2 = [1.5,0,0.5]
+  draw_frame(pos_1)
+  angles_1 = p.calculateInverseKinematics(gripper1,11,pos_1)
+  angles_2 = p.calculateInverseKinematics(gripper2,12,pos_2)
+
+  p.setJointMotorControlArray(gripper1,np.arange(13),controlMode=p.POSITION_CONTROL,
+                                                     targetPositions=angles_1)
+  p.setJointMotorControlArray(gripper2,np.arange(13),controlMode=p.POSITION_CONTROL,targetPositions=angles_2)
+
+  if (t!=0.0):
+    p.addUserDebugLine(prevPose, pos_1, [0, 0, 0.3], 1)
+  prevPose = pos_1
+
+  if(t<=0.5 and reverse!=True):
+    t+=0.01
+  elif(t>0):
+    reverse = True
+    t-=0.01
   else:
-    t = t + 0.01
+    reverse = False
 
-  if (useSimulation and useRealTimeSimulation == 0):
-    p.stepSimulation()
-
-  for i in range(1):
-    pos = [-0.4, 0.2 * math.cos(t), 1 + 0.2 * math.sin(t)]
-    #pos = [1,0,0]
-    #end effector points down, not up (in case useOrientation==1)
-    orn = p.getQuaternionFromEuler([0, -math.pi, 0])
-
-    if (useNullSpace == 1):
-      if (useOrientation == 1):
-        jointPoses = p.calculateInverseKinematics(kukaId, kukaEndEffectorIndex, pos, orn, ll, ul,
-                                                  jr, rp)
-      else:
-        jointPoses = p.calculateInverseKinematics(kukaId,
-                                                  kukaEndEffectorIndex,
-                                                  pos,
-                                                  lowerLimits=ll,
-                                                  upperLimits=ul,
-                                                  jointRanges=jr,
-                                                  restPoses=rp)
-    else:
-      if (useOrientation == 1):
-        jointPoses = p.calculateInverseKinematics(kukaId,
-                                                  kukaEndEffectorIndex,
-                                                  pos,
-                                                  orn,
-                                                  jointDamping=jd,
-                                                  solver=ikSolver,
-                                                  maxNumIterations=100,
-                                                  residualThreshold=.01)
-      else:
-        jointPoses = p.calculateInverseKinematics(kukaId,
-                                                  kukaEndEffectorIndex,
-                                                  pos,
-                                                  solver=ikSolver)
-
-    if (useSimulation):
-      for i in range(numJoints):
-        p.setJointMotorControl2(bodyIndex=kukaId,
-                                jointIndex=i,
-                                controlMode=p.POSITION_CONTROL,
-                                targetPosition=jointPoses[i],
-                                targetVelocity=0,
-                                force=500,
-                                positionGain=0.03,
-                                velocityGain=1)
-    else:
-      #reset the joint state (ignoring all dynamics, not recommended to use during simulation)
-      for i in range(numJoints):
-        p.resetJointState(kukaId, i, jointPoses[i])
-
-  ls = p.getLinkState(kukaId, kukaEndEffectorIndex)
-  if (hasPrevPose):
-    p.addUserDebugLine(prevPose, pos, [0, 0, 0.3], 1, trailDuration)
-    p.addUserDebugLine(prevPose1, ls[4], [1, 0, 0], 1, trailDuration)
-  prevPose = pos
-  prevPose1 = ls[4]
-  hasPrevPose = 1
+  p.stepSimulation()
 p.disconnect()
-'''
