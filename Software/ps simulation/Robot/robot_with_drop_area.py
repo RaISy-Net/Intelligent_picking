df2373a5b0e1b2f26d0003f9a4e30656ad515e7d
Yash Sahijwani <60649571+Terabyte17@users.noreply.github.com>
2020-07-28 14:20:40 +0530

    Add files via upload

--- Software/ps simulation/Robot/robot_with_drop_area.py
+++ Software/ps simulation/Robot/robot_with_drop_area.py
@@ -18,6 +18,7 @@ Area_Halfdim=1
 def get_grasp_prediction(x,y,z,a):
     Robot.rgbd_images(x,y,z)
     network = "C:/Users/yashs/OneDrive/Desktop/Intelligent_picking-master/Software/ps simulation/Robot/trained-models/cornell-randsplit-rgbd-grconvnet3-drop1-ch16/epoch_30_iou_0.97"
+    #network = "C:/Users/yashs/Downloads/epoch_00_iou_0.93"
     rgb_path = "C:/Users/yashs/OneDrive/Desktop/color"+".png"
     depth_path = "C:/Users/yashs/OneDrive/Desktop/depth"+".png"
     gs = predict_grasp_angle(network, rgb_path, depth_path)
@@ -38,9 +39,9 @@ def get_real_world_coord():
     return x,y,angle
 
 def pick(xpos, ypos):
-    Robot.suction_up()
-    Robot.move_frame(ypos+0.06)
-    Robot.move_head(xpos-0.03)
+    Robot.move_frame_and_head(ypos+0.06, xpos-0.03)
+    #Robot.move_frame(ypos+0.06)
+    #Robot.move_head(xpos-0.03)
     z_init = Robot.end_effector()[0][2]
     print(z_init)
     x,y,angle = get_real_world_coord()
@@ -54,8 +55,9 @@ def pick(xpos, ypos):
     Robot.contract_wrist(0.13)
 
 def place(xpos, ypos):
-    Robot.move_frame(ypos+0.06)
-    Robot.move_head(xpos-0.03)
+    Robot.move_frame_and_head(ypos+0.06, xpos-0.03)
+    #Robot.move_frame(ypos+0.06)
+    #Robot.move_head(xpos-0.03)
     Robot.extend_arm()
     #Robot.extend_wrist(0.05)
     Robot.open_gripper()
@@ -66,24 +68,26 @@ x = -0.8
 y = 0.4
 
 Robot = robot()
-p.resetDebugVisualizerCamera(1.8 , 0, -41, [0, -1.4, 1])
-for i in range(25):
+p.resetDebugVisualizerCamera(2 , 0, -41, [0, -1.4, 1])
+Robot.suction_up()
+object_indices = [1, 5, 13, 16, 21]
+count=0
+placing = [[-0.4, 0.8], [0, 0.4], [0.4, 0.12], [0.8, 0.8]]
+time.sleep(2)
+for i in object_indices:
     object = Robot._objectUids[i]
     pos = p.getBasePositionAndOrientation(object)[0]  
     pick(pos[0], pos[1])
     for j in range(181):
-        p.resetDebugVisualizerCamera(1.8, j, -41, [0, -1.4+(2.8*j)/180, 1-j*0.8/180])
+        p.resetDebugVisualizerCamera(2, j, -41, [0, -1.4+(2.8*j)/180, 1-j*0.8/180])
         time.sleep(0.01)
     place(x, y)
     for j in range(181):
-        p.resetDebugVisualizerCamera(1.8 , 180-j, -41, [0, 1.4 - (2.8*j)/180, 0.2 + j*0.8/180])
+        p.resetDebugVisualizerCamera(2 , 180-j, -41, [0, 1.4 - (2.8*j)/180, 0.2 + j*0.8/180])
         time.sleep(0.01)
-    if i%5==4:
-        x = x+0.4
-        y = 0.4
-    else:
-        y = y+0.4
-
+    count+=1
+    x = placing[count][0]
+    y = placing[count][1]
 
 time.sleep(10)
 
