33e7f957097bbd7298026b0c91ad4f6348e3f3b4
Yash Sahijwani <60649571+Terabyte17@users.noreply.github.com>
2020-09-08 17:27:49 +0530

    structure changed

---
+++ Phase_2_submission/Phase_2_codebase/grasp_estimation.py
@@ -0,0 +1,82 @@
+import argparse
+import logging
+
+import matplotlib.pyplot as plt
+import numpy as np
+import torch.utils.data
+from PIL import Image
+import cv2
+
+from hardware.device import get_device
+from inference.post_process import post_process_output
+from utils.data.camera_data import CameraData
+from utils.visualisation.plot import plot_results, save_results
+
+logging.basicConfig(level=logging.INFO)
+
+class GraspEstimation:
+
+    def __init__(self):
+        self.use_depth = 1
+        self.use_rgb = 1 
+        self.n_grasps = 1
+        self.save = 0
+        self.force_cpu = False
+
+    def load_network_image(self, network, rgb_path, depth_path):
+        # Load image
+        logging.info('Loading image...')
+        self.pic = Image.open(rgb_path, 'r')
+        self.rgb = np.array(self.pic)
+        self.pic = Image.open(depth_path, 'r')
+        self.depth = np.expand_dims(np.array(self.pic), axis=2)
+
+        # Load Network
+        logging.info('Loading model...')
+        self.net = torch.load(network,map_location=torch.device('cpu'))
+        logging.info('Done')
+
+    def predict_grasp(self):
+        # Get the compute device
+        self.device = get_device(self.force_cpu)
+
+        self.img_data = CameraData(include_depth=self.use_depth, include_rgb=self.use_rgb)
+
+        x, depth_img, rgb_img = self.img_data.get_data(rgb=self.rgb, depth=self.depth)
+
+        with torch.no_grad():
+            xc = x.to(self.device)
+            pred = self.net.predict(xc)
+
+            q_img, ang_img, width_img = post_process_output(pred['pos'], pred['cos'], pred['sin'], pred['width'])
+            #print(pred['pos'].size())
+            #print(pred['pos'])
+            #print(pred['cos'])
+            #print(pred['sin'])
+            #print(pred['width'])
+            if self.save:
+                save_results(
+                    rgb_img=self.img_data.get_rgb(self.rgb, False),
+                    depth_img=np.squeeze(self.img_data.get_depth(self.depth)),
+                    grasp_q_img=q_img,
+                    grasp_angle_img=ang_img,
+                    no_grasps=n_grasps,
+                    grasp_width_img=width_img
+                )
+            else:
+                fig = plt.figure(figsize=(7, 2))
+                fig.canvas.manager.window.wm_geometry("+330+440")
+                gs=plot_results(fig=fig,
+                            rgb_img=self.img_data.get_rgb(self.rgb, False),
+                            grasp_q_img=q_img,
+                            grasp_angle_img=ang_img,
+                            no_grasps=self.n_grasps,
+                            grasp_width_img=width_img)
+                fig.savefig('img_result.pdf')
+                plt.close()
+                for g in gs:
+                    print(g.center)
+                    print(g.angle)
+                    print(g.length)
+                    print(g.width)
+                return gs
--- Phase_2_submission/Phase_2_codebase/robot_with_drop_area.py
+++ Phase_2_submission/Phase_2_codebase/robot_with_drop_area.py
@@ -11,150 +11,157 @@ import distutils.dir_util
 from pkg_resources import parse_version
 from robot import robot
 from drop_area import *
-from run_offline import predict_grasp_angle
+from grasp_estimation import GraspEstimation
 
 Area_Halfdim=1
 
-def get_grasp_prediction(x,y,z,a):
-    Robot.rgbd_images(x,y,z)
-    network = "./trained models/epoch_17_iou_0.96"
-    rgb_path = "./CapturedImg/color"+".png"
-    depth_path = "./CapturedImg/depth"+".png"
-    gs = predict_grasp_angle(network, rgb_path, depth_path)
-    return gs
+class GridEnvironment():
+    def __init__(self):
+        #loading the robot class
+        self.Robot = robot()
+        self.Robot.suction_up()
+        suction = 0
 
-def get_real_world_coord():
-    end_effector_initpos = Robot.end_effector()[0]
-    a=0
-    gs = get_grasp_prediction(end_effector_initpos[0],end_effector_initpos[1],end_effector_initpos[2],a)
-    x = end_effector_initpos[0]
-    y = end_effector_initpos[1]
-    z = end_effector_initpos[2]
-    a = 0.005
-    y = y - a*(1-gs[0].center[0]/112)
-    x = x + a*(1-gs[0].center[1]/112)
-    score = gs[0].quality
-    angle = gs[0].angle
-    print(x,y)
-    return x,y,angle,score
-
-def pick(xpos, ypos, object, threshold=0.9):
-    Robot.overhead_camera(0)
-    Robot.move_frame_and_head(ypos+0.06, xpos-0.03)
-    z_init = Robot.end_effector()[0][2]
-    x,y,angle,score = get_real_world_coord()
-    if score<threshold:
-        if z_init>1.185:
-            zpos = z_init - 1.181
-            Robot.extend_wrist(zpos)
-        Robot.move_suction_cup(ypos, xpos)
-        Robot.close_gripper(0.09, 1)
-        p.resetDebugVisualizerCamera(0.4, angle, -20, [xpos, ypos, p.getBasePositionAndOrientation(object)[0][2]])
-        Robot.suction_down()
-        cons = Robot.suction_force(object)
-        Robot.suction_up()
+        #reseting the camera
         p.resetDebugVisualizerCamera(2 , 0, -41, [0, -1.4, 1])
-        if z_init>1.185:
-            zpos = z_init - 1.181
-            Robot.contract_wrist(zpos)
-        Robot.reset_gripper()
-        return 1, cons
-    else:
-        Robot.rotate_gripper(angle)
-        Robot.move_frame_and_head(y+0.06, x-0.03)
-        z_init = Robot.end_effector()[0][2]
+        
+        #setting objects to be picked and where to be dropped
+        self.object_indices = [2, 7, 8, 6, 24]  
+        self.count=0
+        self.placing = [[0, 0.8], [0.8, 0.4], [0.8, 0.8]]
+
+        #initializing the grasp model
+        self.GraspModel = GraspEstimation()
+
+    def get_grasp_prediction(self,x,y,z,a):
+        self.Robot.rgbd_images(x,y,z)
+        self.network = "./trained models/epoch_17_iou_0.96"
+        self.rgb_path = "./CapturedImg/color"+".png"
+        self.depth_path = "./CapturedImg/depth"+".png"
+        self.GraspModel.load_network_image(self.network, self.rgb_path, self.depth_path)
+        gs = self.GraspModel.predict_grasp()
+        return gs
+
+    def get_real_world_coord(self):
+        end_effector_initpos = self.Robot.end_effector()[0]
+        a=0
+        gs = self.get_grasp_prediction(end_effector_initpos[0],end_effector_initpos[1],end_effector_initpos[2],a)
+        x = end_effector_initpos[0]
+        y = end_effector_initpos[1]
+        z = end_effector_initpos[2]
+        a = 0.005
+        y = y - a*(1-gs[0].center[0]/112)
+        x = x + a*(1-gs[0].center[1]/112)
+        score = gs[0].quality
+        angle = gs[0].angle
+        return x,y,angle,score
+
+    def pick(self, xpos, ypos, object, threshold=0.9):
+        self.Robot.overhead_camera(0)
+        self.Robot.move_frame_and_head(ypos+0.06, xpos-0.03)
+        z_init = self.Robot.end_effector()[0][2]
+        x,y,angle,score = self.get_real_world_coord()
+        if score<threshold:
+            if z_init>1.185:
+                zpos = z_init - 1.181
+                self.Robot.extend_wrist(zpos)
+            self.Robot.move_suction_cup(ypos, xpos)
+            self.Robot.close_gripper(0.09, 1)
+            p.resetDebugVisualizerCamera(0.4, angle, -20, [xpos, ypos, p.getBasePositionAndOrientation(object)[0][2]])
+            self.Robot.suction_down()
+            cons = self.Robot.suction_force(object)
+            self.Robot.suction_up()
+            p.resetDebugVisualizerCamera(2 , 0, -41, [0, -1.4, 1])
+            if z_init>1.185:
+                zpos = z_init - 1.181
+                self.Robot.contract_wrist(zpos)
+                self.Robot.reset_gripper()
+                return 1, cons
+        else:
+            self.Robot.rotate_gripper(angle)
+            self.Robot.move_frame_and_head(y+0.06, x-0.03)
+            z_init = self.Robot.end_effector()[0][2]
+            zpos = z_init - 1.052
+            p.resetDebugVisualizerCamera(0.4, angle, -20, [xpos, ypos, p.getBasePositionAndOrientation(object)[0][2]])
+            self.Robot.extend_wrist(zpos)
+            self.Robot.close_gripper(0.10)
+            self.Robot.contract_wrist(0.13)
+            p.resetDebugVisualizerCamera(2 , 0, -41, [0, -1.4, 1])
+            self.Robot.reset_gripper()
+            return 0, None
+
+    def place(self, xpos, ypos, suction, cons):
+        self.Robot.overhead_camera(1)
+        if suction:
+            self.Robot.move_suction_cup(ypos, xpos)
+            self.Robot.extend_arm()
+            z = self.Robot.end_effector()[0][2]
+            p.resetDebugVisualizerCamera(0.4, 180, -20, [xpos, ypos, z-0.05])
+            time.sleep(3)
+            self.Robot.remove_suction_force(cons)
+            p.resetDebugVisualizerCamera(2, 180, -41, [0, 1.4, 0.2])
+            self.Robot.contract_arm()
+            self.Robot.open_gripper()
+        else:
+            self.Robot.move_frame_and_head(ypos+0.06, xpos-0.03)
+            self.Robot.extend_arm()
+            z = self.Robot.end_effector()[0][2]
+            p.resetDebugVisualizerCamera(0.4, 180, -20, [xpos, ypos, z-0.05])
+            time.sleep(3)
+            self.Robot.open_gripper()
+            p.resetDebugVisualizerCamera(2, 180, -41, [0, 1.4, 0.2])
+            self.Robot.contract_arm()
+        self.Robot.reset_gripper()
+
+    def grab_drop_suck(self, xpos, ypos, object):
+        self.Robot.move_frame_and_head(ypos+0.06, xpos-0.03)
+        z_init = self.Robot.end_effector()[0][2]
+        x,y,angle,score = self.get_real_world_coord()
+        self.Robot.rotate_gripper(angle)
+        self.Robot.move_frame_and_head(y+0.06, x-0.03)
+        z_init = self.Robot.end_effector()[0][2]
         zpos = z_init - 1.052
         p.resetDebugVisualizerCamera(0.4, angle, -20, [xpos, ypos, p.getBasePositionAndOrientation(object)[0][2]])
-        Robot.extend_wrist(zpos)
-        Robot.close_gripper(0.10)
-        Robot.contract_wrist(0.13)
+        self.Robot.extend_wrist(zpos)
+        self.Robot.close_gripper(0.10)
+        self.Robot.contract_wrist(zpos)
+        p.setJointMotorControl2(Robot.bot, Robot.plate_left, p.VELOCITY_CONTROL, targetVelocity = 0)
+        self.Robot.open_gripper()
+        self.Robot.reset_gripper()
+        pos = p.getBasePositionAndOrientation(object)[0]  
+        ypos = pos[1]
+        xpos = pos[0]
+        self.Robot.reset_gripper()
+        self.Robot.move_suction_cup(ypos, xpos)
+        if z_init>1.185:
+                zpos = z_init - 1.183
+                self.Robot.extend_wrist(zpos)
+        self.Robot.close_gripper(0.09, 1)
+        self.Robot.suction_down()
+        cons = self.Robot.suction_force(object)
+        self.Robot.suction_up()
         p.resetDebugVisualizerCamera(2 , 0, -41, [0, -1.4, 1])
-        Robot.reset_gripper()
-        return 0, None
+        if z_init>1.185:
+            zpos = z_init - 1.181
+            self.Robot.contract_wrist(zpos)
+        self.Robot.reset_gripper()
+        return 1, cons
 
-def place(xpos, ypos, suction, cons):
-    Robot.overhead_camera(1)
-    if suction:
-        Robot.move_suction_cup(ypos, xpos)
-        Robot.extend_arm()
-        z = Robot.end_effector()[0][2]
-        p.resetDebugVisualizerCamera(0.4, 180, -20, [xpos, ypos, z-0.05])
-        time.sleep(3)
-        Robot.remove_suction_force(cons)
-        p.resetDebugVisualizerCamera(2, 180, -41, [0, 1.4, 0.2])
-        Robot.contract_arm()
-        Robot.open_gripper()
-    else:
-        Robot.move_frame_and_head(ypos+0.06, xpos-0.03)
-        Robot.extend_arm()
-        z = Robot.end_effector()[0][2]
-        p.resetDebugVisualizerCamera(0.4, 180, -20, [xpos, ypos, z-0.05])
-        time.sleep(3)
-        Robot.open_gripper()
-        p.resetDebugVisualizerCamera(2, 180, -41, [0, 1.4, 0.2])
-        Robot.contract_arm()
-    Robot.reset_gripper()
-
-
-def grab_drop_suck(xpos, ypos, object):
-    Robot.move_frame_and_head(ypos+0.06, xpos-0.03)
-    z_init = Robot.end_effector()[0][2]
-    x,y,angle,score = get_real_world_coord()
-    Robot.rotate_gripper(angle)
-    Robot.move_frame_and_head(y+0.06, x-0.03)
-    z_init = Robot.end_effector()[0][2]
-    zpos = z_init - 1.052
-    p.resetDebugVisualizerCamera(0.4, angle, -20, [xpos, ypos, p.getBasePositionAndOrientation(object)[0][2]])
-    Robot.extend_wrist(zpos)
-    Robot.close_gripper(0.10)
-    Robot.contract_wrist(zpos)
-    p.setJointMotorControl2(Robot.bot, Robot.plate_left, p.VELOCITY_CONTROL, targetVelocity = 0)
-    Robot.open_gripper()
-    Robot.reset_gripper()
-    pos = p.getBasePositionAndOrientation(object)[0]  
-    ypos = pos[1]
-    xpos = pos[0]
-    Robot.reset_gripper()
-    Robot.move_suction_cup(ypos, xpos)
-    if z_init>1.185:
-            zpos = z_init - 1.183
-            Robot.extend_wrist(zpos)
-    Robot.close_gripper(0.09, 1)
-    Robot.suction_down()
-    cons = Robot.suction_force(object)
-    Robot.suction_up()
-    p.resetDebugVisualizerCamera(2 , 0, -41, [0, -1.4, 1])
-    if z_init>1.185:
-        zpos = z_init - 1.181
-        Robot.contract_wrist(zpos)
-    Robot.reset_gripper()
-    return 1, cons
-
-Robot = robot()
-p.resetDebugVisualizerCamera(2 , 0, -41, [0, -1.4, 1])
-Robot.suction_up()
-object_indices = [1, 7, 8, 6, 24]  
-count=0
-placing = [[0, 0.8], [0.8, 0.4], [0.8, 0.8]]
-suction = 0
-time.sleep(30)
-print(Robot.end_effector())
-for i in  object_indices: 
-    object = Robot._objectUids[i]
-    pos = p.getBasePositionAndOrientation(object)[0]  
-    if i==6:
-        suction, cons = grab_drop_suck(pos[0], pos[1], object)
-    else:
-        suction, cons = pick(pos[0], pos[1], object)
-    for j in range(181):
-        p.resetDebugVisualizerCamera(2, j, -41, [0, -1.4+(2.8*j)/180, 1-j*0.8/180])
-        time.sleep(0.01)
-    x = placing[count][0]
-    y = placing[count][1]
-    count+=1
-    place(x, y, suction, cons)
-    for j in range(181):
-        p.resetDebugVisualizerCamera(2 , 180-j, -41, [0, 1.4 - (2.8*j)/180, 0.2 + j*0.8/180])
-        time.sleep(0.01)
-    
-time.sleep(10)
+    def do_simulation(self):
+        for i in  self.object_indices: 
+            object = self.Robot._objectUids[i]
+            pos = p.getBasePositionAndOrientation(object)[0]  
+            if i==6:
+                suction, cons = self.grab_drop_suck(pos[0], pos[1], object)
+            else:
+                suction, cons = self.pick(pos[0], pos[1], object)
+            for j in range(181):
+                p.resetDebugVisualizerCamera(2, j, -41, [0, -1.4+(2.8*j)/180, 1-j*0.8/180])
+                time.sleep(0.01)
+            x = self.placing[self.count][0]
+            y = self.placing[self.count][1]
+            self.count+=1
+            self.place(x, y, suction, cons)
+            for j in range(181):
+                p.resetDebugVisualizerCamera(2 , 180-j, -41, [0, 1.4 - (2.8*j)/180, 0.2 + j*0.8/180])
+                time.sleep(0.01)
